70e2a5ad105a0fae5d7b465b696b367879faf04b
Ebenhezer <emabotha@gmail.com>
2021-09-01 11:35:56 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SensorsComponent } from './sensors/sensors.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: LoginComponent},
+  {path:'login', component: LoginComponent},
+  {path:'home', component: HomeComponent, canActivate:[AuthGuard]},
+  {path:'sensors', component: SensorsComponent, canActivate:[AuthGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/sensors/sensors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cf-sensors',
+  templateUrl: './sensors.component.html',
+  styleUrls: ['./sensors.component.scss']
+})
+export class SensorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
