1b9d74224e50aad541605a5b22702825e2240187
Ebenhezer <emabotha@gmail.com>
2021-10-09 12:05:21 +0200

    Updated the add button and notification component

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'cf-notifications',
   templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.scss']
+  styleUrls: ['./notifications.component.scss',
+              '../../assets/css/material-dashboard.css']
 })
 export class NotificationsComponent implements OnInit {
 
